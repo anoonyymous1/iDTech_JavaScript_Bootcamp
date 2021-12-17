beec4883deb0cdf256edd0e513aba45c43428f80
anoonyymous1 <92198109+anoonyymous1@users.noreply.github.com>
2021-12-17 23:10:21 +0800

    Add files via upload

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,33 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+var school = "Hogwarts";
+var character = "Harry Potter. ";
+var greetings = "Welcome to Hogwarts! ";
+var worldName = "Wizarding World";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+var year = 1991;
+var students = 1000;
+var friends = 2;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let spell = ['Expelliarmus', 'Petrificus totalus', 'Sectumsempra', 'Reducto', 'Stupefy']
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let favSpell = true;
+
+console.log(greetings + "I'm " + character);
+console.log("I joined " + school + " in " + year + ".");
+console.log(school + " is the most prestigious school in the " + worldName + " and there are " + students + " students that attend " + school + ".");
+console.log("I have " + friends + " very good friends that I made here in " + school + ". Their names are Ron and Hermoine. They have stuck by my side since my frist year here at " + school + "." + "\n")
+console.log("Harry Potter: Does anyone have any questions? " )
+console.log("Me: Do you have a favourite spell?")
+console.log("Harry Potter: " + favSpell + ". My favourite spell is " + spell[0] + "." + "\n")
+console.log("I hope you have a wonderful experience here at Hogwarts!")
+
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
